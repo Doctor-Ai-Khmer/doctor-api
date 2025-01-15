091cc625812bc1dc53f7397c3013bbabc62bfcba
kheang <bunkheangheng99@gmail.com>
2025-01-15 22:29:02 +0700

    set up database with docker

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import * as path from 'path';
 
+
 @Controller()
 export class AppController {
   @Get()
---
+++ src/database/db.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: process.env.DB_SYNC === 'true',
+  logging: true,
+  retryAttempts: 5,
+  retryDelay: 3000,
+};
